3f6636235257f26f04859ad41b9990211294c33e
rayray <119638971+cnulatienpo@users.noreply.github.com>
2025-11-18 02:18:31 -0600

    Recreate Vite bootstrap and new homepage

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 export default defineConfig({
-  server: { host: true, port: 5173, strictPort: false },
+  server: { host: true, port: 5173 },
   preview: { host: true, port: 4173 },
   build: { outDir: 'dist', assetsDir: 'assets' }
 })
