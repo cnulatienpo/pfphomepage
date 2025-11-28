4c0c2945239fd3f346a1b70d547ec17a014ed656
rayray <119638971+cnulatienpo@users.noreply.github.com>
2025-11-28 01:25:01 -0600

    Add files via upload

---
+++ pfp-theme/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExportPanel from './components/ExportPanel';
+
+function App() {
+  return (
+    <div>
+      <h1>Notebook Theme Exporter</h1>
+      <ExportPanel />
+    </div>
+  );
+}
+
+export default App;
