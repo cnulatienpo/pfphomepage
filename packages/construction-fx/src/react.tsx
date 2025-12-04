9db97be9d88a5b7f91b00074632555455b2b7a85
rayray <119638971+cnulatienpo@users.noreply.github.com>
2025-12-04 09:47:27 -0600

    Delete packages/construction-fx directory

--- packages/construction-fx/src/react.tsx
+++
@@ -1,2 +0,0 @@
-export * from "../../../src/lib/construction-fx/react";
-export * from "../../../src/lib/construction-fx/react-wrappers";
--- packages/construction-fx/src/vue.ts
+++
@@ -1,2 +0,0 @@
-export * from "../../../src/lib/construction-fx/vue";
-export * from "../../../src/lib/construction-fx/vue-wrappers";
